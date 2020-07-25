80ab856c51a7e1540f9c06234979cee4fc3118fa
Admin <ilya.steik4@gmail.com>
2020-07-26 01:26:58 +0300

    Updates

---
+++ precache-manifest.017f7ed4cdf6cb6e0146e06bee5c3b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f492f182677072137151051e1ec8b1",
+    "url": "/deathdate/index.html"
+  },
+  {
+    "revision": "8d5a1bdc85ed61ce51fc",
+    "url": "/deathdate/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "a36b742e85a6fee6c024",
+    "url": "/deathdate/static/css/main.86923c83.chunk.css"
+  },
+  {
+    "revision": "8d5a1bdc85ed61ce51fc",
+    "url": "/deathdate/static/js/2.5ba0df6e.chunk.js"
+  },
+  {
+    "revision": "a36b742e85a6fee6c024",
+    "url": "/deathdate/static/js/main.013288e3.chunk.js"
+  },
+  {
+    "revision": "e69c80d786ffcafa6412",
+    "url": "/deathdate/static/js/runtime-main.67627fc5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deathdate/precache-manifest.4abd894a13a821ce477108ececf51244.js"
+  "/deathdate/precache-manifest.017f7ed4cdf6cb6e0146e06bee5c3b5e.js"
 );
 
 self.addEventListener('message', (event) => {
